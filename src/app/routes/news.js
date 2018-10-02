8d97042b096cc683122105a5a50ef7909ce3bfd9
helguera <javier.helguera@alumnos.uva.es>
2018-10-02 21:33:25 +0200

    guarda en bd

--- src/app/routes/news.js
+++ src/app/routes/news.js
@@ -1,13 +1,34 @@
-var dbConnection = require('../../config/dbConnection');
+const dbConnection = require('../../config/dbConnection');
 
 module.exports = app => {
-    connection = dbConnection();
-    app.get('/', (req, res) => {
-        connection.query('SELECT * FROM news', (err, result) => {
-            //console.log(result);
-            res.render('news/news', {
-                news: result
-            })
-        });
+
+  const connection = dbConnection();
+
+  app.delete('/', (req, res) => {
+    connection.query('DELETE FROM news WHERE id=1', (err, result) => {
+        res.redirect('/');
+      });
     });
-}
+  
+
+  app.get('/', (req, res) => {
+    connection.query('SELECT * FROM news', (err, result) => {
+      res.render('news/news', {
+        news: result
+      });
+    });
+  });
+
+  app.post('/', (req, res) => {
+    const { title, news } = req.body;
+    connection.query('INSERT INTO news SET ?',
+      {
+        title,
+        news
+      }
+    , (err, result) => {
+      res.redirect('/');
+    });
+  });
+
+};
