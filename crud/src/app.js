22d8c386157559f31de2948399400c7ed196cf74
helguera <javier.helguera@alumnos.uva.es>
2018-10-04 00:55:24 +0200

    Leer y escribir en la bd

--- crud/src/app.js
+++ crud/src/app.js
@@ -25,6 +25,7 @@ app.use(myConnection(mysql,{
     database: 'crudnodejsmysql'
 }, 'single'));
 
+app.use(express.urlencoded({extended: false}));
 
 //routes
 app.use('/', customerRoutes);
--- crud/src/controllers/customerController.js
+++ crud/src/controllers/customerController.js
@@ -13,4 +13,13 @@ controller.list = (req, res) => {
     });
 };
 
+controller.save = (req, res) => {
+    var data = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('insert into customer set ?', [data], (err, customer) => {
+            res.redirect('/');
+        });
+    })
+}
+
 module.exports = controller;
