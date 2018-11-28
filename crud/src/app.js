fe98342bb18ad3ab2202309e2f5e7994033885a8
helguera <javier.helguera@alumnos.uva.es>
2018-11-28 19:13:12 +0100

    cookies e identificacion de usuarios

--- crud/src/app.js
+++ crud/src/app.js
@@ -5,8 +5,20 @@ const path = require('path');
 const morgan = require('morgan');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
-
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
+
+app.use(session({
+    secret: "sosecret",
+    saveUninitialized: false,
+    resave: false
+  }));
+var sess;
+
+
 
 //importing routes
 const customerRoutes = require('./routes/customer');
@@ -25,17 +37,77 @@ app.use(myConnection(mysql,{
     database: 'crudnodejsmysql'
 }, 'single'));
 
-app.use(express.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'super-secret-key',
+    key: 'super-secret-cookie',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }));
+app.use(flash());
+
 
 //routes
-app.use('/', customerRoutes);
 
+app.get('/admin',function(req,res){
+    sess = req.session;
+    console.log(req.session);
+    if(req.session.mail) {
+        //console.log("ERES ADMIN");
+        res.send("ERES EL PUTO ADMIN");
+    }
+    else {
+        //console.log("NO ERES ADMIN");
+        res.redirect('/');
+    }
+});
+
+app.use('/', customerRoutes);
+app.get('/login', (req, res) => {
+    res.render('login', {
+        data: {},
+        errors: {}
+      })
+});
+
+app.post('/login', (req, res) => {
+    /*res.render('login', {
+        data: req.body, // { message, email }
+        errors: {
+          message: {
+            msg: 'A message is required'
+          },
+          email: {
+            msg: 'That email doesn‘t look right'
+          }
+        }
+      })*/
+    //console.log(req.body.mail);
+    req.session.mail = req.body.mail;
+    req.flash('success', 'Login succesfuly!')
+    res.redirect('/');
+})
+
+app.get('/logout',function(req,res){
+    if(req.session){
+        req.session.destroy(function(err) {
+            if (err) return console.log(err);
+            return res.redirect('/login');
+        });
+    }else{
+        res.redirect('/login');
+    }
+}); 
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 
 //starting server
 app.listen(3000, () => {
